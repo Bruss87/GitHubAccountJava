de08bcd1cae184223e1a804b5d86644eb916bb5f
Bruss87 <kaczmarska.kar@gmail.com>
2019-10-12 22:07:36 +0100

    basic setup and test for new GitHubAccountClass

--- src/main/java/GitHubAccount.java
+++ src/main/java/GitHubAccount.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class GitHubAccount {
+
+    private String userName;
+    private String name;
+    private AccountType accountType;
+    private HashMap<String, Repository> repositories;
+
+    public GitHubAccount(String userName, String name, AccountType accountType) {
+        this.userName = userName;
+        this.name = name;
+        this.accountType = accountType;
+        this.repositories = new HashMap<String, Repository>();
+    }
+
+    public String getUserName() {
+
+        return this.userName;
+    }
+
+    public String getName() {
+
+        return this.name;
+    }
+
+    public AccountType getAccountType() {
+        return this.accountType;
+    }
+    public int countCollectionOfRepos() {
+        return this.repositories.size();
+    }
+//    public void addRepoToCollectionOfRepos(Repository, Repository repository) {
+//        this.repositories.put(, "Repository" );
+//    }
+
+
+
+
 }
--- src/test/java/GitHubAccountTest.java
+++ src/test/java/GitHubAccountTest.java
@@ -1,2 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GitHubAccountTest {
+    private GitHubAccount gitHubAccount;
+    private Repository repository1;
+
+    @Before
+    public void setUp(){
+        gitHubAccount = new GitHubAccount("Zero44","JavaFan", AccountType.FREE);
+        repository1 = new Repository("HotelJava", "practicing usage of multiple classes", RepositoryType.PUBLIC);
+    }
+    @Test
+    public void hasUserName(){
+        assertEquals("Zero44", gitHubAccount.getUserName());
+    }
+    @Test
+    public void hasName(){
+        assertEquals("JavaFan", gitHubAccount.getName());
+    }
+    @Test
+    public void hasAccountType(){
+        assertEquals(AccountType.FREE, gitHubAccount.getAccountType());
+    }
+
+    @Test
+    public void collectionOfReposStartsAtZero(){
+        assertEquals(0, gitHubAccount.countCollectionOfRepos());
+    }
+
+//    @Test
+//    public void canAddRepoToCollectionOfRepos(){
+//        gitHubAccount.addRepoToCollectionOfRepos(repository1);
+//        assertEquals(1, gitHubAccount.countCollectionOfRepos());
+//    }
+
 }
