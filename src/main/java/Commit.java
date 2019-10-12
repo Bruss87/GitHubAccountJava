01a53ccf46a15645695708f4a61ea8ff01620dc8
Bruss87 <kaczmarska.kar@gmail.com>
2019-10-12 12:48:03 +0100

    initilize instances for commit class, add commitType enum

--- src/main/java/Commit.java
+++ src/main/java/Commit.java
@@ -1,2 +1,14 @@
 public class Commit {
+
+    private String description;
+    private CommitType commitType;
+    private int uniqueId;
+
+    public Commit(String description, CommitType commitType, int uniqueId){
+        this.description = description;
+        this.commitType = commitType;
+        this.uniqueId = uniqueId;
+    }
+
+
 }
--- src/test/java/CommitTest.java
+++ src/test/java/CommitTest.java
@@ -1,12 +1,28 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CommitTest {
-    Commit commit1;
+    Commit commit;
 
     @Before
     public void setUp(){
-        commit1 = new Commit("fixed missing values bug",commitType.BUGFIX, 4);
+        commit = new Commit("fixed missing values bug",commitType.BUGFIX, 4);
     }
 
     @Test
+    public void hasDescription(){
+        assertEquals("fixed missing values bug", commit.getDescription());
+    }
+
+    @Test
+    public void hasCommitType(){
+        assertEquals(commitType.BUGFIX, commit.getCommitType());
+    }
+
+    @Test
+    public void commitHasId(){
+        assertEquals(4, commit.getUniqueId());
+    }
 }
